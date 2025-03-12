43e03372567f99c952441fd47443e2e03a4301c1
Edward Su <zentialeds@gmail.com>
2025-03-12 07:38:25 +0000

    dependence setup
    Signed-off-by: Edward Su <zentialeds@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based LaTeX development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [ 
+              node2nix nodejs nodePackages.pnpm yarn
+            ];
+          };
+        }
+      );
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import { IconAtom } from "@tabler/icons-react"
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button><IconAtom/></Button>
+    </div>
+  )
+}
+
+export default App
